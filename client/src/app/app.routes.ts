0ff65d3684ce5e16e458e224d102a7e2dcfd7921
Kingsley <sepenglk@gmail.com>
2024-07-22 13:00:27 +0200

    added nav menu

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,13 +3,18 @@ import {LoginComponent} from "./login/login.component";
 import {SignupComponent} from "./signup/signup.component";
 import {MainpageComponent} from "./mainpage/mainpage.component";
 import {ErrorComponent} from "./error/error.component";
+import {CartComponent} from "./cart/cart.component";
+import {CheckoutComponent} from "./checkout/checkout.component";
+import {LogoutComponent} from "./logout/logout.component";
 
 export const routes: Routes = [
   {path:'',component:LoginComponent},
   {path: 'login',component: LoginComponent},
+  {path:'logout',component: LogoutComponent},
   {path: 'signup',component: SignupComponent},
   {path: 'main',component: MainpageComponent},
-
+  {path: 'cart',component: CartComponent},
+  {path:'checkout',component: CheckoutComponent},
 
   {path:'error',component: ErrorComponent}
 
