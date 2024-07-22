948d5cb251d5245affafc90530601eb115902efe
Kingsley <sepenglk@gmail.com>
2024-07-22 12:27:19 +0200

    project structure

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
+import {MainpageComponent} from "./mainpage/mainpage.component";
+import {ErrorComponent} from "./error/error.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login',component: LoginComponent},
+  {path: 'signup',component: SignupComponent},
+  {path: 'main',component: MainpageComponent},
+
+
+  {path:'error',component: ErrorComponent}
+
+];
+
+
+
